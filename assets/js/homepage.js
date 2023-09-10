bf745dafb92b965022e56ee42f33046731528967
Nikolas Kostiv <nikolaykostiv@gmail.com>
2023-09-10 12:59:48 +0300

    main fixed

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -181,42 +181,4 @@ ppbutton.addEventListener("click", function () {
   }
 });
 
-fraction = 0.99;
-function checkScroll() {
-  var video = el;
-  var x = video.offsetLeft,
-    y = video.offsetTop,
-    w = video.offsetWidth,
-    h = video.offsetHeight,
-    r = x + w, //right
-    b = y + h, //bottom
-    visibleX,
-    visibleY,
-    visible;
-
-  visibleX = Math.max(
-    0,
-    Math.min(
-      w,
-      window.pageXOffset + window.innerWidth - x,
-      r - window.pageXOffset
-    )
-  );
-  visibleY = Math.max(
-    0,
-    Math.min(
-      h,
-      window.pageYOffset + window.innerHeight - y,
-      b - window.pageYOffset
-    )
-  );
-
-  visible = (visibleX * visibleY) / (w * h);
-
-  if (visible > fraction) {
-    ppbutton.src = "./assets/images/play.svg";
-    video.pause();
-  }
-}
-
 /* ======================= Video Play End  =========================== */
